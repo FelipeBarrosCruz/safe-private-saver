b97a1a3d60e1d1c10287c715c4a416fa3046f256
FelipeBarrosCruz <felipe.barros.pt@gmail.com>
2017-02-28 01:33:36 -0300

    Setup configuration to save current tab into chrome storage

--- script/index.js
+++ script/index.js
@@ -1,9 +1,36 @@
-'use strict'
-(function (angular) {
+'use strict';
+(function construct (angular) {
 
-  function construct () {
-    console.log('loaded')
-  }
+  angular.module('SPSApp', [])
+    .service('TabsService', function () {
+      return chrome.tabs;
+    })
+    .service('StorageService', function () {
+      return chrome.storage; 
+    })
+    .controller('IndexController', function IndexController (TabsService, StorageService) {
+    var vm = this;
+    vm.save = save;
 
-  return document.addEventListener('DOMContentLoaded', construct)
-})(angular)
+    function save () {
+      var q = { active:  true, lastFocusedWindow: true };
+      return TabsService.query(q, function (Tabs) {
+        var Tab = Array.isArray(Tabs) && Tabs.length && Tabs[0] || false;
+        if (!Tab) return;
+        StorageService.sync.get('pages', function(value) {
+          var pages = value && Array.isArray(value.pages) && value.pages.length && value.pages || [];
+          var id = CryptoJS.SHA256(Tab.url).toString();
+          if (_.find(pages, {id: id})) return;
+          pages.push({
+            id: id,
+            title: Tab.title,
+            url: Tab.url
+          });
+          StorageService.sync.set({pages: pages})
+        });
+
+      });
+    }
+  });
+
+})(angular);
